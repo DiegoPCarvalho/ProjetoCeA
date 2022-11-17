8a0acad18fc2ac53b22e1545c34c790a470cf5ef
Diego Pontes de Carvalho <diegopontescarvalho@outloo.com>
2022-11-17 14:42:20 -0300

    fase inicial novamente

--- frontend/src/Components/Home/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Nav from "../Template/Nav";
-
-export default props => 
-<div>
-    <h1>olá mundo</h1>
-</div>
--- frontend/src/Components/Template/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './Footer.css';
-import React from 'react';
-
-export default props => 
-    <footer className="footer">
-        <span>
-            Desenvolvido por <strong>DC<span className='text-danger'>7</span> </strong>
-        </span>
-    </footer>
