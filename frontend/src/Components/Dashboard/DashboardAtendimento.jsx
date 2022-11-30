6420c9b03b2d484e73580bdc52c532bf99c960b9
Diego Pontes de Carvalho <diegopontescarvalho@outloo.com>
2022-11-30 14:27:44 -0300

    fase 8.3

--- frontend/src/Components/Dashboard/DashboardAtendimento.jsx
+++ frontend/src/Components/Dashboard/DashboardAtendimento.jsx
@@ -1,7 +1,9 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import Main from '../Template/Main';
 import Card from 'react-bootstrap/Card';
 import './Dashboard.css';
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 
 const headerProps = {
@@ -10,6 +12,31 @@ const headerProps = {
 }
 
 export default class DashAtendimentoOnSite extends Component {
+
+    state = { isOpen: false }
+
+    openModal = () => this.setState({ isOpen: true })
+    closeModal = () => this.setState({ isOpen: false })
+
+    renderModal() {
+        return (
+            <>
+                    <Button variant="primary" onClick={this.openModal}>
+                        Launch demo modal
+                    </Button>
+                <Modal show={this.state.isOpen} onHide={this.closeModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary">Close</Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        );
+    }
+
     Card(texto, cor, bgCor, numero, estado) {
         return (
             <Card border={bgCor} bg={cor} style={{ width: '18rem' }}>
@@ -24,6 +51,7 @@ export default class DashAtendimentoOnSite extends Component {
         )
     }
 
+
     render() {
         return (
             <Main {...headerProps}>
@@ -31,6 +59,7 @@ export default class DashAtendimentoOnSite extends Component {
                     <div className='d-flex text-light'>
                         <div className='sombra'>
                             {this.Card('Total Atendimento', 'primary', '', 50, 'Finalizada')}
+                            {/* {this.renderModal()} */}
                         </div>
                     </div>
 
